18dee21cb30c5f39c3687b2a8f261075e6f5faf1
Suman Biswas <suman.lalbagh93@gmail.com>
2022-08-06 15:08:55 +0530

    registration data store in database by async await

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const express = require('express');
+const express = require('express');  
 const app = express();
 
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 require('./db/conn');
-const User = require('./model/userSchema');
 
 app.use(express.json());
-
 app.use(require('./router/auth'));
 
 const PORT = process.env.PORT;
-
 app.listen(PORT, () => {
     console.log(`server is running at port no. ${PORT}`);
 });
--- router/auth.js
+++ router/auth.js
@@ -10,21 +10,45 @@ router.get('/about', (req, res) => {
     res.send('Hello world about');
 });
 
-router.post('/register', (req, res) => {
-    const { name, email, phone, work, password, cpassword } = req.body;
 
-    if (!name || !email || !phone || !work || !password || !cpassword) {
-        return res.status(422).json({ msg: 'Please enter all fields' });
+// user registation using async-await
+router.post('/register', async (req,res) => {
+    const {name, email, phone, work, password, cpassword} = req.body;
+    
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error: "Please filled all field properly"});
     }
+    try{
+        const userExist = await User.findOne({email: email});
+        if(userExist){
+            res.status(422).json({error:"User already Existed"});
+        }
+        const user = User({name, email, phone, work, password, cpassword});
+        await user.save();
+        res.status(201).json({message:"Registration successfull"})
+    }catch(err){ console.log(err);}
+})
 
-    User.findOne({ email: email })
-        .then((userExist) => {
-            if (userExist) {
-                return res.status(422).json({ msg: 'User already exist' });
-            }
-            const user = new User({ name, email, phone, work, password, cpassword });
-            user.save().then((user) => res.json(user)).catch((err) => console.log(err));
-        }).catch((err)=> console.log(err));
-});
+
+// user registation using promises..... 
+// router.post('/register', (req, res) => {
+//     const { name, email, phone, work, password, cpassword } = req.body;
+
+//     if (!name || !email || !phone || !work || !password || !cpassword) {
+//         return res.status(422).json({ msg: 'Please enter all fields' });
+//     }
+
+//     User.findOne({ email: email })
+//         .then((userExist) => {
+//             if (userExist) {
+//                 return res.status(422).json({ msg: 'User already exist' });
+//             }
+//             const user = new User({ name, email, phone, work, password, cpassword });
+//             user.save().then((user) => res.json(user)).catch((err) => console.log(err));
+//         }).catch((err)=> console.log(err));
+// });
+
+
+
 module.exports = router;
 
